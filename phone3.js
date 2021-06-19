fbe1120d222e1cc90e7ffa9bfd586f5cdf6b80fb
Your Name <you@example.com>
2021-06-19 11:40:47 +0800

    delete wrong build fiel

--- phone3.js
+++
@@ -1,8 +0,0 @@
-
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./phone3.cjs.production.min.js')
-} else {
-  module.exports = require('./phone3.cjs.development.js')
-}
